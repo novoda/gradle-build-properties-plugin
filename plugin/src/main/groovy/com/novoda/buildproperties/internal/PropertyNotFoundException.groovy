cb6810d0e324ac6cedf5b0060ea92f514d391de2
Antonio Bertucci <archano@gmail.com>
2017-07-17 00:29:28 +0200

    Remove unused exception

--- plugin/src/main/groovy/com/novoda/buildproperties/internal/PropertyNotFoundException.groovy
+++
@@ -1,9 +0,0 @@
-package com.novoda.buildproperties.internal
-
-class PropertyNotFoundException extends IllegalArgumentException {
-
-    PropertyNotFoundException(String key) {
-        super("No property defined with key '$key'")
-        //super("No value defined for property '$key' in properties file $file.absolutePath")
-    }
-}
