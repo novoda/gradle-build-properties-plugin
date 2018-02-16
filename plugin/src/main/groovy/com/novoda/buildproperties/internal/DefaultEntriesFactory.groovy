ecd9ca51769ffe7cb725edefe5cad5ea60a35a01
Antonio Bertucci <archano@gmail.com>
2018-02-16 10:45:17 +0000

    Introduce entries factory

---
+++ plugin/src/main/groovy/com/novoda/buildproperties/internal/DefaultEntriesFactory.groovy
@@ -0,0 +1,30 @@
+package com.novoda.buildproperties.internal
+
+import com.novoda.buildproperties.BuildProperties
+import com.novoda.buildproperties.Entries
+import com.novoda.buildproperties.ExceptionFactory
+import org.gradle.api.GradleException
+
+class DefaultEntriesFactory implements Entries.Factory {
+
+    private final ExceptionFactory exceptionFactory
+
+    DefaultEntriesFactory(ExceptionFactory exceptionFactory) {
+        this.exceptionFactory = exceptionFactory
+    }
+
+    @Override
+    Entries from(def source) {
+        if (source instanceof BuildProperties) {
+            return source.entries
+        }else if (source instanceof Entries) {
+            return source as Entries
+        } else if (source instanceof Map<String, Object>) {
+            return new MapEntries(source, exceptionFactory)
+        } else if (source instanceof File) {
+            return FilePropertiesEntries.create(source, exceptionFactory)
+        } else {
+            throw new GradleException("Unsupported type of source (${source.class})")
+        }
+    }
+}
