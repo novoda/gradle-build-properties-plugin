8572d16209490239d2a7169d83c911f32d9661fc
Tobias Heine <tobias@novoda.com>
2018-03-08 16:15:35 +0100

    Deprecate method

--- plugin/src/main/groovy/com/novoda/buildproperties/BuildProperties.groovy
+++ plugin/src/main/groovy/com/novoda/buildproperties/BuildProperties.groovy
@@ -70,6 +70,7 @@ class BuildProperties {
         newChain(project)
     }
 
+    @Deprecated
     EntriesChain file(File file, String description = '') {
         logger.warn("""/!\\ WARNING /!\\ Detected use of 'file' method to consume build properties from a file. 
                        |                 This api is deprecated and will be removed in an upcoming release, please use using() instead.
